d314fd4d31ba8d3d65570039295bb3bff452979b
Biffo89 <nicholasbell97@gmail.com>
2021-11-26 23:44:14 +1300

    First commit, looks like everything's working. Basic overlay with contribution points and rum points, along with the total.

--- src/main/java/com/biffo89/troublebrewing/TroubleBrewingPlugin.java
+++ src/main/java/com/biffo89/troublebrewing/TroubleBrewingPlugin.java
@@ -2,15 +2,17 @@ package com.biffo89.troublebrewing;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.*;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
 
 @Slf4j
 @PluginDescriptor(
@@ -18,16 +20,28 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class TroubleBrewingPlugin extends Plugin
 {
+
 	@Inject
 	private Client client;
 
 	@Inject
 	private TroubleBrewingConfig config;
 
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Inject
+	private TroubleBrewingOverlay troubleBrewingOverlay;
+
+	@Getter
+	@Setter
+	private int resourcePoints, bottles;
+
 	@Override
-	protected void startUp() throws Exception
+	protected void startUp()
 	{
 		log.info("Trouble Brewing started!");
+		overlayManager.add(troubleBrewingOverlay);
 	}
 
 	@Override
@@ -37,13 +51,7 @@ public class TroubleBrewingPlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Trouble Brewing says " + config.greeting(), null);
-		}
-	}
+	public void onGameStateChanged(GameStateChanged gameStateChanged) {}
 
 	@Provides
 	TroubleBrewingConfig provideConfig(ConfigManager configManager)
