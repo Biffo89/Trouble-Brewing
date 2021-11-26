8d49016254fee63d50a1d681fd51e369b5c80d00
Biffo89 <nicholasbell97@gmail.com>
2021-11-26 14:06:43 +1300

    Add files via upload

---
+++ src/main/java/com/biffo89/troublebrewing/TroubleBrewingConfig.java
@@ -0,0 +1,19 @@
+package com.biffo89.troublebrewing;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("troublebrewing")
+public interface TroubleBrewingConfig extends Config
+{
+	@ConfigItem(
+		keyName = "greeting",
+		name = "Welcome Greeting",
+		description = "The message to show to the user when they login"
+	)
+	default String greeting()
+	{
+		return "Hello";
+	}
+}
