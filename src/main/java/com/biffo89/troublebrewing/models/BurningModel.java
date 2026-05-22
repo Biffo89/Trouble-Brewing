e823a445993da8169aeae6ce0e74f5e4c5a91a86
Jacob Beu <jacobbeu@gmail.com>
2026-05-22 09:27:18 -0500

    boilers are reversed for blue team adds highlight to all burning bridges
    (cherry picked from commit f641833eb5193ba48da046ce64d8251650bed3e8)

--- src/main/java/com/biffo89/troublebrewing/models/BurningModel.java
+++ src/main/java/com/biffo89/troublebrewing/models/BurningModel.java
@@ -11,9 +11,16 @@ public class BurningModel extends TeamImageModel {
 
     private static final int BUCKET_OF_WATER_ID = 1929;
 
+    private static final int[] BURNING_BRIDGES_OBJECT_IDS = { 15857, 15856, 15883, 15882 };
+
     public BurningModel(TroubleBrewingPlugin plugin, Client client, GameObject gameObject) {
         super(plugin, client, gameObject);
         imageId = BUCKET_OF_WATER_ID;
         outlineColor = Color.RED;
+
+        if (Arrays.stream(BURNING_BRIDGES_OBJECT_IDS).anyMatch(id -> gameObject.getId() == id))
+        {
+            outlineIgnoreTeam = true;
+        }
     }
 }
